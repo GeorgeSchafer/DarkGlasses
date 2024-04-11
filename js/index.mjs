8b2f7674c8d210cf98fc7d1590584f0c6ca75120
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-04-11 02:34:40 -0700

    preparing to release dev changes to beta

---
+++ js/index.mjs
@@ -0,0 +1,22 @@
+import {
+    display,
+    event,
+    unit,
+
+    selections,
+
+    H1,
+    H2,
+    H3,
+    P,
+    Div,
+    FlexBox,
+    Form,
+    Label,
+    Select,
+    Option,
+    Input,
+    Button,
+    Listener,
+} from './mod/vanilla.mjs'
+
